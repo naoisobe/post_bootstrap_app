db4d1351f994101ad9739e0165eabc7d7ff30397
n.isobe <ganmodoki7.43dari@gmail.com>
2020-10-01 16:28:30 +0900

    エラーメッセージを日本語化

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -23,6 +24,8 @@ module PostBootstrapApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    config.i18n.default_locale = :ja
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
